bcec5a849e5e305b326c376c1419d41b6f0bf57e
mmehtapavci <mmmehtapavci@gmail.com>
2024-03-30 23:42:39 +0300

    edit

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 
-const searchImages = () => {
-    axios.get('https://api.unsplash.com/search/photos')
+const searchImages = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+            Authorization: 'Client-ID zpLfc-RBoU2mrLbKxM4kUXh5pl4MUr0FqTRfFBJ234g'
+        },
+        params: {
+            query: term,
+        }
+    });
+    return response.data.results;
 };
+export default searchImages;
